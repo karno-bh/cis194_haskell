728b5443ee809ef643c5c955619d71ba475f5fad
karno <karno.bh@gmail.com>
2023-06-27 06:17:51 +0000

    _refactor_: skips function inner lambda parameters should be patten matched

--- week03/Golf.hs
+++ week03/Golf.hs
@@ -3,17 +3,23 @@ module Golf where
 -- skips :: [a] -> [[a]]
 -- skips l = take (length l) (repeat l)
 
-st1 l = zip [1..] (take (length l) (repeat l))
+-- st1 l = zip [1..] (take (length l) (repeat l))
+-- 
+-- st2 l = map (\el -> ((fst el), (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
+-- 
+-- st3 l = map (\el -> filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el))) (zip [1..] (take (length l) (repeat l)))
+-- 
+-- st4 l = map (\el -> map (\e -> snd e) (filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
+-- 
+-- st5 l = map (\el -> map snd (filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
+-- 
+-- st6 l = takeWhile (/= []) $ map (\el -> map snd $ filter (\x -> ((fst x) `mod` (fst el)) == 0) $ zip [1..] $ snd el) $ zip [1..] $ repeat l
+-- 
+-- st7 l = takeWhile (/= []) $ map (\(n, lst) -> map snd $ filter (\(i, _) -> i `mod` n == 0) $ zip [1..] $ lst) $ zip [1..] $ repeat l
+-- 
+-- skips l = st7 l
 
-st2 l = map (\el -> ((fst el), (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
+skips = takeWhile (/= []) $ map (\(n, lst) -> map snd $ filter (\(i, _) -> i `mod` n == 0) $ zip [1..] $ lst) $ zip [1..] $ repeat l
 
-st3 l = map (\el -> filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el))) (zip [1..] (take (length l) (repeat l)))
 
-st4 l = map (\el -> map (\e -> snd e) (filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
 
-st5 l = map (\el -> map snd (filter (\x -> ((fst x) `mod` (fst el)) == 0) (zip [1..] (snd el)))) (zip [1..] (take (length l) (repeat l)))
-
-st6 l = takeWhile (/= []) $ map (\el -> map snd $ filter (\x -> ((fst x) `mod` (fst el)) == 0) $ zip [1..] $ snd el) $ zip [1..] $ repeat l
-
-skips :: [a] -> [[a]]
-skips = st6
